5c409d8dd2e8c7b1543ca0aecae7c8462062d66d
Carlos Larriu <larriucarlos@gmail.com>
2023-02-01 18:56:50 +0100

    added basic config for the project

--- virus_total_telegram_bot/config.py
+++ virus_total_telegram_bot/config.py
@@ -3,38 +3,61 @@ Configuration file
 """
 import os
 import sys
+import logging
 
 import structlog
 
-from virus_total_telegram_bot.entities import Configuration
+from virus_total_telegram_bot.entities import Config
 
 
-def load_loggers(logs_file_path: str):
+logger = structlog.get_logger()
+
+
+def load_loggers(logs_path: str):
     """
     Load the loggers
-    
+
     Parameters:
     -----------
-    - logs_file_path: str
-      The path where the logs file is going to be stored.
+    - logs_path: str
+      The path where the log files are going to be stored.
     """
-    logger = structlog.get_logger()
-    structlog.configure(processors=[structlog.processors.JSONRenderer(),structlog.stdlib.ProcessorFormatter.wrap_for_formatter],
-                        logger_factory=structlog.stdlib.LoggerFactory(),
-                        wrapper_class=structlog.stdlib.BoundLogger,
-                        cache_logger_on_first_use=True,
-                        context_class=dict,
-                        logger_name='virus_total_telegram_bot',
-                        wrapper_kwargs={'output_file':logs_file_path}
-                        )
+    log_file_path = f"{logs_path}/virus_total.log"
+    # Configure terminal output
+    structlog.configure(
+        processors=[
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
+            structlog.processors.JSONRenderer()
+        ],
+        context_class=structlog.threadlocal.wrap_dict(dict),
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
+    # Configure json file output
+    file_handler = logging.FileHandler(log_file_path)
+    formatter = logging.Formatter('%(message)s')
+    file_handler.setFormatter(formatter)
+    structlog.get_logger().addHandler(file_handler)
 
 
-def load_configuration():
+def create_application_directories(working_directory: str):
     """
-    Load configuration function
+    Creates the needed directories for the application to work.
+
+    Parameters:
+    -----------
+    - working_directory: str
+      The work directory of the bot. From this root, all artefacts will be organized and saved.
+
+    Returns:
+    --------
+    - artifacts_path: str
+      The path where the application artifacts will be saved.
+    - logs_path: str
+      The path where the applications log files will be saved.
     """
-    logger.info("load_configuration")
-    working_directory = os.getenv("WORKING_DIRECTORY", "/tmp")
     logger.info("bot_working_directory", path=working_directory)
     artifacts_path = f"{working_directory}/artifacts"
     logs_path = f"{working_directory}/logs"
@@ -44,9 +67,33 @@ def load_configuration():
     except OSError:
         logger.error("make_directory", artifacts_path=artifacts_path, logs_path=logs_path)
         sys.exit(1)
+    return artifacts_path, logs_path
 
-    BOT_API_KEY = os.getenv("VIRUS_TOTAL_APIKEY")
-    config = Configuration(
-        
+
+def load_configuration(artifacts_path: str, logs_file_path: str):
+    """
+    Load configuration function.
+
+    Parameters:
+    -----------
+    - artifacts_path: str
+        The path where the artifacts are going to be stored.
+    - logs_file_path: str
+        The path where the logs are going to be stored.
+
+    Returns:
+    --------
+    - config: Config
+      The configuration object.
+    """
+    logger.info("loading_configuration")
+
+    VIRUS_TOTAL_BOT_APIKEY = os.getenv("VIRUS_TOTAL_BOT_APIKEY")    # pylint: disable=invalid-name
+    VIRUS_TOTAL_APIKEY = os.getenv("VIRUS_TOTAL_APIKEY")            # pylint: disable=invalid-name
+    config = Config(
+        artifacts_path=artifacts_path,
+        logs_path=logs_file_path,
+        bot_apikey=VIRUS_TOTAL_BOT_APIKEY,
+        virus_total_apikey=VIRUS_TOTAL_APIKEY
     )
     return config
--- virus_total_telegram_bot/entities.py
+++ virus_total_telegram_bot/entities.py
@@ -1,5 +1,14 @@
 """
 All the entities needed for the bot. Here you will find all data structires.
 """
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 
+class Config(BaseModel):
+    """
+    Class to store the application configuration
+    """
+    artifacts_path: str
+    logs_path: str
+    bot_apikey: str
+    virus_total_apikey: str
