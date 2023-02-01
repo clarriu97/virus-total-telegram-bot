df5963773ea4157b85d69067064915fe2a37e464
Carlos Larriu <larriucarlos@gmail.com>
2023-02-01 18:55:54 +0100

    added start callback

---
+++ virus_total_telegram_bot/callbacks.py
@@ -0,0 +1,7 @@
+"""All Bot Callbacks"""
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
