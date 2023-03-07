6f336e27a51a5428066ec1334b98f9fb726e2762
Carlos Larriu <larriucarlos@gmail.com>
2023-03-07 22:58:05 +0100

    modified the config to adapt it to structlog

--- virus_total_telegram_bot/config.py
+++ virus_total_telegram_bot/config.py
@@ -10,10 +10,7 @@ import structlog
 from virus_total_telegram_bot.entities import Config
 
 
-logger = structlog.get_logger()
-
-
-def load_loggers(logs_path: str):
+def load_loggers():
     """
     Load the loggers
 
@@ -22,52 +19,43 @@ def load_loggers(logs_path: str):
     - logs_path: str
       The path where the log files are going to be stored.
     """
-    log_file_path = f"{logs_path}/virus_total.log"
-    # Configure terminal output
+    # configure structlog to work with two loggers: one for the console and one for a json file
+    # the console logger will have plain text and the json logger will have json
     structlog.configure(
-        processors=[
-            structlog.stdlib.add_log_level,
-            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
-            structlog.processors.JSONRenderer()
-        ],
-        context_class=structlog.threadlocal.wrap_dict(dict),
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.stdlib.BoundLogger,
-        cache_logger_on_first_use=True,
+      processors=[
+              structlog.contextvars.merge_contextvars,
+              structlog.processors.add_log_level,
+              structlog.processors.StackInfoRenderer(),
+              structlog.dev.set_exc_info,
+              structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+              structlog.dev.ConsoleRenderer()
+          ],
+          wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+          context_class=dict,
+          logger_factory=structlog.PrintLoggerFactory(),
+          cache_logger_on_first_use=False
     )
-    # Configure json file output
-    file_handler = logging.FileHandler(log_file_path)
-    formatter = logging.Formatter('%(message)s')
-    file_handler.setFormatter(formatter)
-    structlog.get_logger().addHandler(file_handler)
 
 
-def create_application_directories(working_directory: str):
+def create_application_directories(logs_path: str, artifacts_path: str):
     """
     Creates the needed directories for the application to work.
 
     Parameters:
     -----------
-    - working_directory: str
-      The work directory of the bot. From this root, all artefacts will be organized and saved.
-
-    Returns:
-    --------
-    - artifacts_path: str
-      The path where the application artifacts will be saved.
     - logs_path: str
-      The path where the applications log files will be saved.
+      The path where the log files are going to be stored.
+    - artifacts_path: str
+      The path where the artifacts are going to be stored.
     """
-    logger.info("bot_working_directory", path=working_directory)
-    artifacts_path = f"{working_directory}/artifacts"
-    logs_path = f"{working_directory}/logs"
+    logger = structlog.get_logger()
     try:
         os.makedirs(artifacts_path, exist_ok=True)
         os.makedirs(logs_path, exist_ok=True)
+        logger.info("paths_created", artifacts_path=artifacts_path, logs_path=logs_path)
     except OSError:
-        logger.error("make_directory", artifacts_path=artifacts_path, logs_path=logs_path)
+        logger.error("make_path", artifacts_path=artifacts_path, logs_path=logs_path)
         sys.exit(1)
-    return artifacts_path, logs_path
 
 
 def load_configuration(artifacts_path: str, logs_file_path: str):
@@ -86,6 +74,7 @@ def load_configuration(artifacts_path: str, logs_file_path: str):
     - config: Config
       The configuration object.
     """
+    logger = structlog.get_logger()
     logger.info("loading_configuration")
 
     VIRUS_TOTAL_BOT_APIKEY = os.getenv("VIRUS_TOTAL_BOT_APIKEY")    # pylint: disable=invalid-name
