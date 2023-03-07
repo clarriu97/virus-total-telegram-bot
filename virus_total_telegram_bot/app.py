80ed8aa791aa247cb978e6868f7ea966efc13c4c
Carlos Larriu <larriucarlos@gmail.com>
2023-03-07 22:57:33 +0100

    adapted the app to the new library version and added handlers for start, text and files

--- virus_total_telegram_bot/app.py
+++ virus_total_telegram_bot/app.py
@@ -1,9 +1,11 @@
 """Entrypoint of the app"""
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 from virus_total_telegram_bot.entities import Config
 from virus_total_telegram_bot.callbacks import (
-    start
+    start,
+    text,
+    file
 )
 
 
@@ -19,6 +21,11 @@ def run(cfg: Config):
     application = ApplicationBuilder().token(cfg.bot_apikey).build()
 
     start_handler = CommandHandler('start', start)
+    text_handler = MessageHandler(filters.TEXT, text)
+    file_handler = MessageHandler(filters.Document.ALL, file)
+    
     application.add_handler(start_handler)
+    application.add_handler(text_handler)
+    application.add_handler(file_handler)
 
     application.run_polling()
