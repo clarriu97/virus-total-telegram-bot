51718d636aafa851e6634dc9979f7f8a33414d28
Carlos Larriu <larriucarlos@gmail.com>
2023-03-07 22:55:55 +0100

    added the text responses file

---
+++ virus_total_telegram_bot/strings.py
@@ -0,0 +1,15 @@
+"""Strings used as the text of the Bot"""
+# pylint: disable=anomalous-backslash-in-string
+
+ENGLISH = 'en'
+SPANISH = 'es'
+
+dialogs = {
+    "start": {
+        ENGLISH:
+            "💻 Hi there! Welcome to Virus Total 🔍, I am an unofficial Telegram bot that uses the "
+            "VirusTotal API (https://virustotal.com) to perform security analysis on URLs and files. "
+            "I can help you check the safety and integrity of your files and URLs. You can send me a "
+            "URL or a file (up to 30 megabytes) and I will give you the results of the analysis. 😊",
+    }
+}
