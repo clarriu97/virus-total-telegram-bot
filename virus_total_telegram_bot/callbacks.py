66c2c3327cb1cf4da1592fcd38166b397e8c1e14
Carlos Larriu <larriucarlos@gmail.com>
2023-03-07 22:58:52 +0100

    adapted the start callback and added new callbacks for text and files

--- virus_total_telegram_bot/callbacks.py
+++ virus_total_telegram_bot/callbacks.py
@@ -2,6 +2,28 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from virus_total_telegram_bot.utils import (
+    request_arrived
+)
+from virus_total_telegram_bot.strings import dialogs, ENGLISH
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    request_arrived(update, context, command="/start")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=dialogs['start'][ENGLISH])
+
+
+async def text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    request_arrived(update, context, command="text")
+    # get the text received from the user
+    text = update.message.text
+    # send the text back to the user
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+
+async def file(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    request_arrived(update, context, command="file")
+    # get the file received from the user
+    file = update.message.document
+    # send the file back to the user
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=file.file_id)
