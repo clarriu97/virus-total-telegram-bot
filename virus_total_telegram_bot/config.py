62e6fc9b6032953f731d417c1132cfc76cc5b5b0
Carlos Larriu <larriucarlos@gmail.com>
2023-04-27 19:34:44 +0200

    added logging config for console and json but not working with structlog logs

--- virus_total_telegram_bot/config.py
+++ virus_total_telegram_bot/config.py
@@ -3,14 +3,74 @@ Configuration file
 """
 import os
 import sys
-import logging
+import logging.config
+
+from socket import gethostname
+from getpass import getuser
 
 import structlog
 
 from virus_total_telegram_bot.entities import Config
 
 
-def initialize_loggers():
+def extract_from_record(_, __, event_dict):
+    """
+    Extract thread and process names and add them to the event dict.
+    """
+    record = event_dict["_record"]
+    event_dict["thread_name"] = record.threadName
+    event_dict["process_name"] = record.processName
+
+    return event_dict
+
+
+def add_hostname(_, __, event_dict):
+    """
+    Add hostname to the event dict.
+
+    Parameters:
+    -----------
+    - logger: structlog.stdlib.BoundLogger
+    - method_name: str
+    - event_dict: dict
+        The event dict.
+
+    Returns:
+    --------
+    - event_dict: dict
+        The event dict with the hostname added.
+    """
+    record = event_dict.get("host")
+    if record is None:
+        event_dict["host"] = gethostname()
+
+    return event_dict
+
+
+def add_user(_, __, event_dict):
+    """
+    Add username to the event dict.
+
+    Parameters:
+    -----------
+    - logger: structlog.stdlib.BoundLogger
+    - method_name: str
+    - event_dict: dict
+        The event dict.
+
+    Returns:
+    --------
+    - event_dict: dict
+        The event dict with the username added.
+    """
+    record = event_dict.get("user")
+    if record is None:
+        event_dict["user"] = getuser()
+
+    return event_dict
+
+
+def initialize_loggers(logs_path: str):
     """
     Load the loggers
 
@@ -19,21 +79,74 @@ def initialize_loggers():
     - logs_path: str
       The path where the log files are going to be stored.
     """
-    # configure structlog to work with two loggers: one for the console and one for a json file
-    # the console logger will have plain text and the json logger will have json
+    log_file = os.path.join(logs_path, "virus_total_telegram_bot.log")
+    timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
+    pre_chain = [
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.ExtraAdder(),
+        timestamper,
+    ]
+
+    logging.config.dictConfig({
+      "version": 1,
+      "disable_existing_loggers": False,
+      "formatters": {
+          "json": {
+              "()": structlog.stdlib.ProcessorFormatter,
+              "processors": [
+                add_hostname,
+                add_user,
+                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                structlog.processors.JSONRenderer()
+              ],
+              "foreign_pre_chain": pre_chain,
+          },
+          "console": {
+              "()": structlog.stdlib.ProcessorFormatter,
+              "processors": [
+                add_hostname,
+                add_user,
+                structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                structlog.dev.ConsoleRenderer(colors=True),
+              ],
+              "foreign_pre_chain": pre_chain,
+          },
+      },
+      "handlers": {
+          "default": {
+              "level": "DEBUG",
+              "class": "logging.StreamHandler",
+              "formatter": "console",
+          },
+          "file": {
+              "level": "INFO",
+              "class": "logging.handlers.WatchedFileHandler",
+              "filename": log_file,
+              "formatter": "json",
+          },
+      },
+      "loggers": {
+          "": {
+              "handlers": ["default", "file"],
+              "level": "DEBUG",
+              "propagate": True,
+          },
+      }
+    })
+
     structlog.configure(
-      processors=[
-              structlog.contextvars.merge_contextvars,
-              structlog.processors.add_log_level,
-              structlog.processors.StackInfoRenderer(),
-              structlog.dev.set_exc_info,
-              structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
-              structlog.dev.ConsoleRenderer()
-      ],
-      wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
-      context_class=dict,
-      logger_factory=structlog.PrintLoggerFactory(),
-      cache_logger_on_first_use=False
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.StackInfoRenderer(),
+            structlog.dev.set_exc_info,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M.%S"),
+            structlog.dev.ConsoleRenderer()
+        ],
+        wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+        context_class=dict,
+        logger_factory=structlog.PrintLoggerFactory(),
+        cache_logger_on_first_use=False
     )
 
 
@@ -48,13 +161,10 @@ def create_application_directories(logs_path: str, artifacts_path: str):
     - artifacts_path: str
       The path where the artifacts are going to be stored.
     """
-    logger = structlog.get_logger()
     try:
         os.makedirs(artifacts_path, exist_ok=True)
         os.makedirs(logs_path, exist_ok=True)
-        logger.info("paths_created", artifacts_path=artifacts_path, logs_path=logs_path)
     except OSError:
-        logger.error("make_path", artifacts_path=artifacts_path, logs_path=logs_path)
         sys.exit(1)
 
 
